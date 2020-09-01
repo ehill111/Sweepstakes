80077eb36c0b509fa4e550043025e7d1606959cb
Eric M Hill <ehill111@comcast.net>
2020-08-31 21:52:59 -0400

    1. Created new member variables for Sweepstakes class: companySelection, contestantSelection, and winningTicket. 2. Instantiated dictionary in constructor of Sweepstakes class. 3. Added dictionary to RegisterContestant. 4. Wrote logic for PickWinner method for Sweepstakes class.

--- Sweepstakes1/Contestant.cs
+++ Sweepstakes1/Contestant.cs
@@ -19,7 +19,7 @@ namespace Sweepstakes1
         
 
         //Methods do something.
-
+        public 
 
 
 
--- Sweepstakes1/Sweepstakes.cs
+++ Sweepstakes1/Sweepstakes.cs
@@ -11,25 +11,60 @@ namespace Sweepstakes1
         private Dictionary<int, Contestant> contestants;
         private string name;
         public string Name;
-        
+        int companySelection;
+        int contestantSelection;
+        int winningTicket;
 
         //Properties
 
         //Constructor
         public Sweepstakes(string name)
         {
-            
+            Dictionary<int, Contestant> contestant = new Dictionary<int, Contestant>(); 
         }
         //Methods do something.
         public void RegisterContestant(Contestant contest)
         {
             //Add registrant to contestant list.
             Dictionary<int, Contestant> contestants = new Dictionary<int, Contestant>();
-            //contestants.Add(Userinterface.RequestContestantContactData);
+            contestants.Add(Userinterface.RequestContestantContactData);//need help
         }
         public void PickWinner()
         {
+            //Console.WriteLine("Select a number from 1 to 1,000,000 and press enter. If you select the same number that will be chosen on New Year's Eve, you win $1,000,000. Type in your number and press Enter.");
+            //int contestantSelection = int.Parse(Console.ReadLine());
+            //Console.ReadLine([contestantSelection]);
+            //Random rng = new Random();
+            //companySelection = rng.Next(1, 1000001);
+            //Sweepstakes winningTicket = new Sweepstakes();
 
+            //int companySelection = 053221;
+            //if(contestantSelection == companySelection)
+            //{
+            //    Console.WriteLine("Congratulations! You win one million dollars!");
+            //}
+            //else if(contestantSelection != companySelection)
+            //{
+            //    Console.WriteLine("Oh, shucks! Your number didn't win this time.");
+            //}
+
+            switch (053221)
+            {
+                case 094420:
+                    break;
+                case 883099:
+                    break;
+                case 053221:
+                    Console.WriteLine("Congratulation!" + 053221 + "is the winning ticket! You win one million dollars!");
+                    break;
+                case 222209:
+                    break;
+                case 737389:
+            }
+            
+
+                     
+                    
         } 
 
         public void PrintContestantInfo(Contestant contestant)
